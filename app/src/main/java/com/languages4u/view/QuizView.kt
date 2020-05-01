7e51f3774bce9922af917edbb1f9c52d9519b71e
JacekGajewski <jacek.gajewski@ymail.com>
2020-05-01 11:17:45 +0200

    Sketch UI for Quiz created

---
+++ app/src/main/java/com/languages4u/view/QuizView.kt
@@ -0,0 +1,24 @@
+package com.languages4u.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.languages4u.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class QuizView : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_view, container, false)
+    }
+
+}
